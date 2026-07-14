9f198be8f59bcde32c9e05b993d4e269812653d8
Adrian Esteban Camposeco Galvez <acamposeco-2024018@kinal.edu.gt>
2026-07-14 10:30:54 -0600

    feat/ T63-agrega configuracion global en cors-configuration.js

---
+++ ProdifyBackend-B/config/cors-configuration.js
@@ -0,0 +1,12 @@
+const corsOptions = {
+
+    origin: true,
+
+    credentials: true,
+
+    methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+
+    allowedHeaders: "Content-Type, Authorization"
+}
+
+export { corsOptions }
