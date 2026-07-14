8f57ebb1e1e742d12af63c43ac6eb134ed5b8dcc
Marioescobar64 <mescobar-2022471@kinal.edu.gt>
2026-07-14 11:56:04 -0600

    refactor: Exporta nuevas APIs de tareas y productividad centralizadas

--- ProdifyFronted/src/shared/api/index.js
+++ ProdifyFronted/src/shared/api/index.js
@@ -2,3 +2,5 @@ export * from "./auth";
 export * from "./admin";
 export { axiosAuth, axiosBackendA, axiosBackendB, handleRefreshToken } from "./api";
 export * from "./api";
+export * from "./task.api";
+export * from "./productivity.api";
